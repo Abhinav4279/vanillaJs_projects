bb9afd4b8e4423cbe241a3ef168574a17a0e9f81
Abhinav4279 <sharmaabhinav4279@gmail.com>
2022-01-27 19:26:27 +0530

    Feature Add: search based movies

--- movies/script.js
+++ movies/script.js
@@ -1,13 +1,24 @@
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const mainEl = document.querySelector('main');
+const formEl = document.querySelector('form');
+const searchEl = document.getElementById('search');
 
-async function getMovies() {
-    const resp = await fetch(APIURL);
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
 
-    respData.results.forEach(movieData => {
+    showList(respData);
+}
+
+function showList(movies) {
+    mainEl.innerHTML = '';
+
+    movies.results.forEach(movieData => {
         const movie = document.createElement('div');
         movie.className = "movie";
 
@@ -21,8 +32,12 @@ async function getMovies() {
 
         mainEl.appendChild(movie);
     });
-
-
-    return respData;
 }
-getMovies();
+
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const searchTerm = searchEl.value;
+
+    getMovies(SEARCHAPI + searchTerm);
+    searchEl.value = '';
+});
